ae521047c5e77611831d8344f8d70ee41da6b662
Theodore Kelechukwu Onyejiaku <theodoreonyejiaku@gmail.com>
2022-12-07 16:02:44 +0100

    Update base url for backend api

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,2 +1,3 @@
-export const baseUrl = "https://campus-foodie-api.herokuapp.com/"
+// export const baseUrl = "https://campus-foodie-api.herokuapp.com/"
 // export const baseUrl = "http://localhost:5000/"
+export const baseUrl = "https://campus-foodie-api.onrender.com/"
